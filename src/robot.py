448c779b5b88f2fe7ba87af13c5970bfb829ad97
Cameron Reikes <cameronreikes@gmail.com>
2019-03-05 18:37:41 -0800

    Add basic robot with logging

---
+++ src/robot.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import magicbot
+import wpilib
+import logging
+import coloredlogs
+
+
+class Robot(magicbot.MagicRobot):
+
+    def createObjects(self):
+        """Creates component objects assosciated with hardware, run on robot init"""
+        field_styles = coloredlogs.DEFAULT_FIELD_STYLES
+        field_styles['filename'] = {'color': 'cyan'}
+        coloredlogs.install(level='DEBUG',
+                            fmt="%(asctime)s[%(msecs)d] %(filename)s:%(lineno)d %(name)s %(levelname)s %(message)s", datefmt="%m-%d %H:%M:%S", field_styles=field_styles)  # install to created handler
+
+    def teleopInit(self):
+        """Called on first launch of teleop mode"""
+        pass
+
+    def teleopPeriodic(self):
+        """Called on every packet received by the robot, about 60 times per second"""
+        pass
+
+
+# Run magicbot when executed as a script
+if __name__ == '__main__':
+    wpilib.run(Robot)
